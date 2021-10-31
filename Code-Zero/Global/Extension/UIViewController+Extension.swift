db0342e3f71e0ab0dc1f316c974b3d9911b88562
Juhyeoklee <gur6531@naver.com>
2021-10-31 18:49:29 +0900

    [:art: style] add UIViewController Extension method
    feat: #7

---
+++ Code-Zero/Global/Extension/UIViewController+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIViewController+Extension.swift
+//  Code-Zero
+//
+//  Created by 이주혁 on 2021/10/31.
+//
+
+import UIKit
+
+
+extension UIViewController {
+    // UIAlertController without handler
+    func simpleAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인",style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    // UIAlertController with Handler
+    func simpleAlertwithHandler(title: String, message: String, okHandler : ((UIAlertAction) -> Void)?, cancleHandler : ((UIAlertAction) -> Void)?){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인",style: .default, handler: okHandler)
+        let cancelAction = UIAlertAction(title: "취소",style: .cancel, handler: cancleHandler)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // Set NavigationBar Background Clear without underline
+    func setNavigationBarClear() {
+        let bar: UINavigationBar! = self.navigationController?.navigationBar
+        
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor.clear
+    }
+    
+    // Set NavigationBar Background Clear with underline
+    func setNavigationBarShadow() {
+        let bar: UINavigationBar! = self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.backgroundColor = UIColor.white
+    }
+}
