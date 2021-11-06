f3153dc0427c7184420ab180d781e5301278cd58
xwoud <xwoud@naver.com>
2021-11-06 19:42:33 +0900

    [:lipstick: layout] calendar today's appear sytle
    feat: #10

--- Code-Zero/Calendar/VC/CalendarVC.swift
+++ Code-Zero/Calendar/VC/CalendarVC.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import FSCalendar
 
 class CalendarVC: UIViewController {
 
+    // MARK: - @IBOutlet
+
+    @IBOutlet weak var scrollView: UIView!
+    @IBOutlet weak var calendar: FSCalendar!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension CalendarVC {
+
+    func setView() {
+
+        self.view.backgroundColor = .darkGray2
+        scrollView.backgroundColor = .darkGray2
+        calendar.backgroundColor = .darkGray2
+
+        // title: Day
+        calendar.appearance.titleDefaultColor = .gray2
+        calendar.appearance.titleWeekendColor = .gray2
+        calendar.appearance.titleFont = .spoqaHanSansNeo(size: 14, family: .medium)
+
+        // headerTitle: 달력 이름
+        calendar.appearance.headerTitleColor = .white
+        calendar.appearance.headerTitleFont = .futuraStd(size: 20, family: .heavy)
+
+        // weekday: 요일
+        calendar.appearance.weekdayTextColor = .gray2
+        calendar.appearance.weekdayFont = .futuraStd(size: 13, family: .heavy)
+
+       
+        calendar.appearance.todaySelectionColor = .white
+        calendar.select(Date())
     }
-    */
-
 }
