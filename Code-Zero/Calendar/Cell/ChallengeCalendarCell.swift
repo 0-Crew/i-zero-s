e028f9a9e2c4db163fe4f35acfb30e69aa7d31bb
xwoud <xwoud@naver.com>
2021-11-13 19:59:48 +0900

    [:wrench: configure] calendar cell 두가지 종류 추가
    feat: #10

---
+++ Code-Zero/Calendar/Cell/ChallengeCalendarCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChallengeCalendarCell.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2021/11/13.
+//
+
+import Foundation
+
+import FSCalendar
+
+class ChallengeCalendarCell: FSCalendarCell {
+    
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+    }
+}
+    
---
+++ Code-Zero/Calendar/Cell/TodayCalendarCell.swift
@@ -0,0 +1,37 @@
+//
+//  TodayCalendarCell.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2021/11/13.
+//
+
+import UIKit
+import FSCalendar
+
+class TodayCalendarCell: FSCalendarCell {
+    
+    weak var underLine: UIView!
+    
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        let underLine: UIView = UIView()
+        self.contentView.insertSubview(underLine, at: 0)
+        self.underLine = underLine
+        self.underLine.backgroundColor = .white
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.underLine.frame = self.contentView.bounds
+        self.underLine.frame = CGRect(x: self.contentView.frame.width/2-5,
+                                      y: self.contentView.frame.height/2 + 5,
+                                      width: 10,
+                                      height: 2)
+        
+    }
+}
