2a841fc5f531434cdd34511e0625f93185fdc2fc
xwoud <xwoud@naver.com>
2021-11-19 11:17:53 +0900

    [:lipstick:layout] 달력 날짜 기본 textColor 지정
    feat: #10

--- Code-Zero/Screen/Calendar/Cell/ChallengeCalendarCell.swift
+++ Code-Zero/Screen/Calendar/Cell/ChallengeCalendarCell.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
-
 import FSCalendar
 
 class ChallengeCalendarCell: FSCalendarCell {
+
+    var selectionType: SelectedType = .none {
+        didSet {
+            setNeedsLayout()
+        }
+    }
 
     required init!(coder aDecoder: NSCoder!) {
         fatalError("init(coder:) has not been implemented")
@@ -19,5 +24,18 @@ class ChallengeCalendarCell: FSCalendarCell {
 
         super.init(frame: frame)
 
+        let view = UIView(frame: self.bounds)
+        self.backgroundView = view
+
+    }
+
+    override func configureAppearance() {
+        super.configureAppearance()
+
+        if self.isPlaceholder { // 현재 달력에 보이는 이전 달, 다음 달 날짜들
+            self.titleLabel.textColor = .gray4
+        } else { // 이번달 날짜들
+            self.titleLabel.textColor = .gray3
+        }
     }
 }
