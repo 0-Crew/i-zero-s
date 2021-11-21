4d9240d299a25582c477c82ea13791d28697f9d6
Juhyeoklee <gur6531@naver.com>
2021-11-21 17:01:02 +0900

    [:art: style] add collection view extension
    - deque method를 추가

--- Code-Zero/Global/Extension/UICollectionView+Extension.swift
+++ Code-Zero/Global/Extension/UICollectionView+Extension.swift
@@ -9,14 +9,26 @@ import UIKit
 
 extension UICollectionView {
 
+    // Custom Cell 등록 하는 extension
     func registerCell(cellType: UICollectionViewCell.Type, resuseIdentifier: String? = nil) {
         let reuseIdentifier = resuseIdentifier ?? String(describing: cellType.self)
         self.register(cellType, forCellWithReuseIdentifier: reuseIdentifier)
     }
 
+    // Custom Cell 등록 하는 extension - xib 등록시 사용
     func registerCell(nibName: String, resuseIdentifier: String? = nil) {
         let identifier = resuseIdentifier ?? nibName
         let nibInstance = UINib(nibName: nibName, bundle: nil)
         self.register(nibInstance, forCellWithReuseIdentifier: identifier)
+    }
+
+    func dequeueCell<T: UICollectionViewCell>(reuseIdentifier: String? = nil, indexPath: IndexPath) -> T {
+        let reuseIdentifier = reuseIdentifier ?? String(describing: T.self)
+        guard
+            let cell = self.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? T
+        else {
+            fatalError()
+        }
+        return cell
     }
 }
