39b52dcf7b6480b29f862399dd69e62d846a94eb
xwoud <xwoud@naver.com>
2021-11-25 00:28:09 +0900

    [:art: style] 코드 형태 개선
    if else -> switch문으로 변경 및 self 삭제
    
    feat: #17

--- Code-Zero/Screen/Calendar/Cell/TodayCalendarCell.swift
+++ Code-Zero/Screen/Calendar/Cell/TodayCalendarCell.swift
@@ -12,7 +12,7 @@ class TodayCalendarCell: FSCalendarCell {
 
     weak var underLine: UIView!
 
-    var selectionType: SelectedType = .none {
+    internal var selectionType: SelectedType = .none {
         didSet {
             setNeedsLayout()
         }
@@ -23,30 +23,29 @@ class TodayCalendarCell: FSCalendarCell {
     }
 
     override init(frame: CGRect) {
-
         super.init(frame: frame)
         let underLine: UIView = UIView()
-        self.contentView.insertSubview(underLine, at: 1) // 지정한 인덱스의 view 삽입
+        contentView.insertSubview(underLine, at: 1) // 지정한 인덱스의 view 삽입
         self.underLine = underLine
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.underLine.frame = self.contentView.bounds
-        self.underLine.frame = CGRect(x: self.contentView.frame.width/2-5,
-                                      y: self.contentView.frame.height/2 + 5,
-                                      width: 10,
-                                      height: 2)
+        underLine.frame = contentView.bounds
+        underLine.frame = CGRect(x: contentView.frame.width/2-5,
+                                 y: contentView.frame.height/2 + 5,
+                                 width: 10,
+                                 height: 2)
 
-        if selectionType == .none {
-            self.titleLabel.textColor = .gray1
-            self.underLine.backgroundColor = .white
+        switch selectionType {
+        case .none:
+            titleLabel.textColor = .gray1
+            underLine.backgroundColor = .white
+        case .selected:
+            titleLabel.textColor = .darkGray2
+            underLine.backgroundColor = .darkGray2
+        case .challenge(_):
+            break
         }
-
-        if selectionType == .selected {
-            self.titleLabel.textColor = .darkGray2
-            self.underLine.backgroundColor = .darkGray2
-        }
-
     }
 }
