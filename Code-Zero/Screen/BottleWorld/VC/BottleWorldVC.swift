b31e26aa9c3b8ec864d463ebe4da146b2a5f764a
Hailey <mini.kim@carrotins.com>
2021-12-03 15:12:35 +0900

    [:wrench: configure] BottleWorld View 만들 VC 생성

---
+++ Code-Zero/Screen/BottleWorld/VC/BottleWorldVC.swift
@@ -0,0 +1,15 @@
+//
+//  BottleWorldVC.swift
+//  Code-Zero
+//
+//  Created by Hailey on 2021/12/03.
+//
+
+import UIKit
+
+class BottleWorldVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
