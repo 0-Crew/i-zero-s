6cdc0385c014cfdec81283cdfd07fec928f1d989
xwoud <xwoud@naver.com>
2021-12-03 15:30:40 +0900

    [:lipstick: layout] 페이지 뷰 CollectionView Property 생성

--- Code-Zero/Screen/BottleWorld/VC/BottleWorldVC.swift
+++ Code-Zero/Screen/BottleWorld/VC/BottleWorldVC.swift
@@ -6,10 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class BottleWorldVC: UIViewController {
 
+    // MARK: - Property
+    lazy var pageCollectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 100, height: 100), collectionViewLayout: collectionViewLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: - View Layout Style
+extension BottleWorldVC {
+    func setPageCollectionViewLayout() {
+//        pageCollectionView.register(UINib(nibName: 셀이름.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: PageCell.reusableIdentifier)
+        view.addSubview(pageCollectionView)
+        pageCollectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+//        pageCollectionView.topAnchor.constraint(equalTo: self.customMenuBar.bottomAnchor).isActive = true
     }
 }
