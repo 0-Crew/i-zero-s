e87064ca39d69f1b696372adac148b9e7d04fe3a
Juhyeoklee <gur6531@naver.com>
2021-12-04 21:53:15 +0900

    [:lipstick: layout] make xib views each step
    - 챌린지 오픈 단계를 xib 로 변경
    
    feat: 챌린지/오픈 #22

---
+++ Code-Zero/Screen/ChallengeOpen/View/ChallengeOpenSecondStepView.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeOpenSecondStepView.swift
+//  Code-Zero
+//
+//  Created by 주혁 on 2021/12/04.
+//
+
+import UIKit
+
+class ChallengeOpenSecondStepView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Code-Zero/Screen/ChallengeOpen/View/ChallengeOpenThirdStepView.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeOpenSecondThirdView.swift
+//  Code-Zero
+//
+//  Created by 주혁 on 2021/12/04.
+//
+
+import UIKit
+
+class ChallengeOpenSecondThirdView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
