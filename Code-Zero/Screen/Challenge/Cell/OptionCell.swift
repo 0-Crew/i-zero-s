ea1843c3543b22be76e96761f82ecd5cefffa2b0
Juhyeoklee <gur6531@naver.com>
2021-12-11 23:31:00 +0900

    [:art: style] fix option cell reusable
    - 챌린지 선택지 cell을 재사용 가능하게 수정
    
    feat: 챌린지/오픈 #22

--- Code-Zero/Screen/Challenge/Cell/OptionCell.swift
+++ Code-Zero/Screen/Challenge/Cell/OptionCell.swift
@@ -8,35 +8,59 @@
 import UIKit
 
 class OptionCell: UITableViewCell {
+    enum CellType {
+        case challenge
+        case challengeOpen
+    }
     let optionTextLabel: UILabel = {
         let label = UILabel()
         label.textColor = .darkGray1
         label.font = .spoqaHanSansNeo(size: 12, family: .medium)
         return label
     }()
+    internal var cellType: CellType!
+
+    // MARK: - Lifecycle Method
     override func awakeFromNib() {
         super.awakeFromNib()
         initLayout()
     }
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         initLayout()
     }
-
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         initLayout()
     }
-
     private func initLayout() {
-        selectionStyle = .none
         contentView.addSubview(optionTextLabel)
         optionTextLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(5)
             $0.bottom.equalToSuperview().offset(-5)
             $0.leading.equalToSuperview().offset(12)
             $0.trailing.equalToSuperview().offset(-17)
+        }
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if cellType == .challenge { return }
+        if selected {
+            optionTextLabel.font = .spoqaHanSansNeo(size: 14, family: .bold)
+        } else {
+            optionTextLabel.font = .spoqaHanSansNeo(size: 14, family: .medium)
+        }
+    }
+    internal func setCellType(type: CellType) {
+        cellType = type
+        if type == .challenge {
+            selectionStyle = .none
+            selectedBackgroundView = nil
+        } else {
+            selectionStyle = .default
+            let highlightingView: UIView = .init()
+            highlightingView.backgroundColor = .lightGray1
+            selectedBackgroundView = highlightingView
         }
     }
 }
