4f03b7a243f096c5fcd78ada81638e5da347f241
xwoud <xwoud@naver.com>
2021-12-12 01:30:21 +0900

    [:lipstick: layout] 탭바 하단 뷰 기본 searchBar 생성
    feat: #26

--- Code-Zero/Screen/BottleWorld/Cell/BottleWorldListCell.swift
+++ Code-Zero/Screen/BottleWorld/Cell/BottleWorldListCell.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class BottleWorldListCell: UICollectionViewCell {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchResultView: UIView!
+
+    // MARK: - Override Fucntion
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.backgroundColor = .gray
-        // Initialization code
+        setView()
+    }
+
+    // MARK: - Style Set Function
+    private func setView() {
+        self.backgroundColor = .white
+        searchView.makeRounded(cornerRadius: 10)
+        searchButton.titleLabel?.text = ""
     }
 
 }
