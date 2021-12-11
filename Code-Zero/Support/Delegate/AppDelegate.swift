18b3f0962f96a7388be536af3f088c089c6632d6
minnit <51286963+xwoud@users.noreply.github.com>
2021-12-12 00:47:06 +0900

    [:chart_with_upwards_trend: analytics] 레이아웃 깨짐 분석해주는 Gedatsu 추가 (#27)

--- Code-Zero/Support/Delegate/AppDelegate.swift
+++ Code-Zero/Support/Delegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Gedatsu
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? ) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+            // Override point for customization after application launch.
+            Gedatsu.open()
+            return true
+        }
 
     // MARK: UISceneSession Lifecycle
 
@@ -23,10 +25,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration(name: "Default Configuration",
+                                        sessionRole: connectingSceneSession.role)
+        }
 
     func application(_ application: UIApplication,
                      didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
