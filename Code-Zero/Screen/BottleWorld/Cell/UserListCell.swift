e2899a9b6dc6814e0a7e4712d395783881ab472a
xwoud <xwoud@naver.com>
2021-12-13 01:36:27 +0900

    [:lipstick: layout] userListCell 정보 받아와서 뿌리기 함수 제작
    feat: #29

--- Code-Zero/Screen/BottleWorld/Cell/UserListCell.swift
+++ Code-Zero/Screen/BottleWorld/Cell/UserListCell.swift
@@ -9,15 +9,50 @@ import UIKit
 
 class UserListCell: UITableViewCell {
 
+    // MARK: - @IBOutlet
+    @IBOutlet weak var bottleBackView: UIView!
+    @IBOutlet weak var bottleImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var challengeTermLabel: UILabel!
+    @IBOutlet weak var challengeLabel: UILabel!
+    @IBOutlet weak var followButton: UIButton!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        setView()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // MARK: - Style Set Function
+    private func setView() {
+        bottleBackView.makeRounded(cornerRadius: nil)
+        bottleBackView.setBorder(borderColor: .lightGray1, borderWidth: 1)
+        followButton.setBorder(borderColor: .orangeMain, borderWidth: 1)
     }
-    
+    func setUserInfo(userName: String, term: String, challenge: String, isFollow: Bool) {
+        userNameLabel.text = "\(userName)의 보틀"
+        challengeTermLabel.text = term
+        challengeLabel.text = challenge
+        isFollow ? setFollowButton() : setFollowingButton()
+        if let text = userNameLabel.text {
+            let attributedStr = NSMutableAttributedString(string: text)
+            attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String),
+                                       value: UIFont.futuraStd(size: 13, family: .bold),
+                                       range: (text as NSString).range(of: userName))
+            userNameLabel.attributedText = attributedStr
+        }
+    }
+    private func setFollowButton() {
+        followButton.setButton(text: "팔로우",
+                               color: .white,
+                               font: .spoqaHanSansNeo(size: 12, family: .bold),
+                               backgroundColor: .orangeMain)
+    }
+    private func setFollowingButton() {
+        followButton.setButton(text: "팔로잉",
+                               color: .orangeMain,
+                               font: .spoqaHanSansNeo(size: 12, family: .bold),
+                               backgroundColor: .white)
+
+    }
 }
