ae0c9a208e8f9c050c4fb2f6df8e5f505a879b27
xwoud <xwoud@naver.com>
2021-12-13 00:57:37 +0900

    [:wrench: configure] user table view를 구성할 xib 파일 생성
    feat: #29

---
+++ Code-Zero/Screen/BottleWorld/Cell/UserListCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserListCell.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2021/12/13.
+//
+
+import UIKit
+
+class UserListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
