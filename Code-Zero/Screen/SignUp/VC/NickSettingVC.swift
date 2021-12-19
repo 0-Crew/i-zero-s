5d0ed4fe49c36851f55f37fa6f53cb12cf96e17f
xwoud <xwoud@naver.com>
2021-12-19 23:42:51 +0900

    [:wrench: configure] 회원가입 후 닉네임 정하는 뷰 생성
    feat: #33

---
+++ Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
@@ -0,0 +1,29 @@
+//
+//  NickSettingVC.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2021/12/19.
+//
+
+import UIKit
+
+class NickSettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
