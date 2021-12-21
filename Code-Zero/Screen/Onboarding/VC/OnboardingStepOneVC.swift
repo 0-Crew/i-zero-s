e4cf939e78af8cd2f68f624527c0ed5ceacb5a1a
Juhyeoklee <gur6531@naver.com>
2021-12-21 20:39:02 +0900

    [:lipstick: layout] 온보딩 1단계 애니메이션 설정
    feat: 온보딩1,2 #35

--- Code-Zero/Screen/Onboarding/VC/OnboardingStepOneVC.swift
+++ Code-Zero/Screen/Onboarding/VC/OnboardingStepOneVC.swift
@@ -9,8 +9,36 @@ import UIKit
 
 class OnboardingStepOneVC: UIViewController {
 
+    @IBOutlet var descriptionLabels: [UILabel]!
+    @IBOutlet weak var nextButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initView()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startAnimation()
+    }
+
+    private func initView() {
+        descriptionLabels[1...2].forEach {
+            $0.alpha = 0
+        }
+        nextButton.alpha = 0
+    }
+
+    private func startAnimation() {
+        animateAlpha(view: descriptionLabels[1]) { _ in
+            self.animateAlpha(view: self.descriptionLabels[2]) { _ in
+                self.animateAlpha(view: self.nextButton)
+            }
+        }
+    }
+
+    private func animateAlpha(view: UIView, completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: 0.8, animations: {
+            view.alpha = 1
+        }, completion: completion)
     }
 }
