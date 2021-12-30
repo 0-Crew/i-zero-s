ee12512c1ab7bbac8d743c0ba5d3434cf8f75ade
xwoud <xwoud@naver.com>
2021-12-31 00:54:48 +0900

    [:sparkles: feature] 닉네임 글자수 중복 검사 및 문자 제한
    feat: #33

--- Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
+++ Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
@@ -9,21 +9,74 @@ import UIKit
 
 class NickSettingVC: UIViewController {
 
+    // MARK: - @IBOutlet
+    @IBOutlet weak var nickBoxView: UIView!
+    @IBOutlet weak var nickTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var duplicateCheckLabel: UILabel!
+
+    // MARK: - @IBAction
+    @IBAction func nextButtonDidTap(_ sender: UIButton) {
+        if nickTextField.text != "" {
+            if let text = nickTextField.text {
+                print(checkDuplicateNick(nick: text))
+            }
+        }
+    }
+    // MARK: - Property
+    var duplicateNick: [String] = ["민희", "주혁", "보틀월드"]
+
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setView()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
     }
-    */
+}
 
+// MARK: - View Layout Style
+extension NickSettingVC {
+    func setView() {
+        nickBoxView.setBorder(borderColor: .gray2, borderWidth: 1)
+        duplicateCheckLabel.isHidden = true
+        nextButton.backgroundColor = .gray2
+        nextButton.tintColor = .gray3
+        nickTextField.delegate = self
+        nickTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    func checkDuplicateNick(nick: String) -> Bool {  // 중복검사(서버로 대체 예정)
+        return duplicateNick.contains(nick)
+    }
+    func filterNickName(text: String) -> String? {
+        do {
+            let regex = try NSRegularExpression(pattern: "[가-힣ㄱ-ㅎㅏ-ㅣA-Za-z0-9!_]")
+            let results = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }.joined(separator: "")
+
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension NickSettingVC: UITextFieldDelegate {
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        textField.text = textField.text?.trimmingCharacters(in: .whitespaces) // 스페이스 제거
+        if let text = textField.text {
+            if let regexText = filterNickName(text: text) {
+                textField.text = regexText
+            }
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(false)
+        return true
+    }
 }
