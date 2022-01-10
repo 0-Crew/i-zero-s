9edae6efbfc9939db0813d8776ed14f933ca0529
xwoud <xwoud@naver.com>
2022-01-10 16:33:10 +0900

    [:sparkles: feature] 닉네임 검사 후 버튼 활성화 및 비활성화 기능 추가
    feat: #33

--- Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
+++ Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 class NickSettingVC: UIViewController {
 
     // MARK: - @IBOutlet
     @IBOutlet weak var nickBoxView: UIView!
     @IBOutlet weak var nickTextField: UITextField!
-    @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var duplicateCheckLabel: UILabel!
 
     // MARK: - @IBAction
@@ -25,25 +25,39 @@ class NickSettingVC: UIViewController {
     }
     // MARK: - Property
     var duplicateNick: [String] = ["민희", "주혁", "보틀월드"]
+    lazy var accessoryView: UIView = {
+        return UIView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: 50.0))
+    }()
+    var nextButton: UIButton = {
+        let button = UIButton()
+        button.setButton(text: "확인",
+                         color: .gray3,
+                         font: .spoqaHanSansNeo(size: 16, family: .bold),
+                         backgroundColor: .red)
+        button.backgroundColor = .gray2
+        return button
+    }()
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setView()
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-        self.view.endEditing(true)
-    }
 }
 
 // MARK: - View Layout Style
 extension NickSettingVC {
     func setView() {
+        nickTextField.becomeFirstResponder()
         nickBoxView.setBorder(borderColor: .gray2, borderWidth: 1)
         duplicateCheckLabel.isHidden = true
-        nextButton.backgroundColor = .gray2
-        nextButton.tintColor = .gray3
         nickTextField.delegate = self
+        nickTextField.inputAccessoryView = accessoryView
+        accessoryView.addSubview(nextButton)
+        nextButton.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+
         nickTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
     }
     func checkDuplicateNick(nick: String) -> Bool {  // 중복검사(서버로 대체 예정)
@@ -74,9 +88,21 @@ extension NickSettingVC: UITextFieldDelegate {
                 textField.text = regexText
             }
         }
+
+        if let count = textField.text?.count {
+            if count > 0 && count < 6 {
+                nextButton.backgroundColor = .orangeMain
+                nextButton.setTitleColor(.white, for: .normal)
+                nextButton.isEnabled = true
+            } else {
+                nextButton.backgroundColor = .gray2
+                nextButton.setTitleColor(.gray3, for: .normal)
+                nextButton.isEnabled = false
+            }
+        }
     }
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.endEditing(false)
         return true
     }
 }
