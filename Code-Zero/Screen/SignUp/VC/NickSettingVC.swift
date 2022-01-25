e4695b7c6c821d3ba6ddfa6a4ca7d635d515e7c4
xwoud <xwoud@naver.com>
2022-01-25 17:24:12 +0900

    [:poop: poop] 닉네임 글자수 제한 똥쌌습니다
    feat: #33

--- Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
+++ Code-Zero/Screen/SignUp/VC/NickSettingVC.swift
@@ -80,33 +80,81 @@ extension NickSettingVC {
             return nil
         }
     }
+
+    private func textLimit(existingText: String?,
+                           newText: String,
+                           limit: Int) -> Bool {
+        let text = existingText ?? ""
+        let isAtLimit = text.count + newText.count <= limit
+        return isAtLimit
+    }
 }
 
 // MARK: - UITextFieldDelegate
 extension NickSettingVC: UITextFieldDelegate {
     @objc func textFieldDidChange(_ textField: UITextField) {
         duplicateCheckLabel.isHidden = true
-        textField.text = textField.text?.trimmingCharacters(in: .whitespaces) // 스페이스 제거
-        if let text = textField.text {
-            if let regexText = filterNickName(text: text) {
-                textField.text = regexText
-            }
-        }
 
-        if let count = textField.text?.count {
-            if count > 0 && count < 6 {
-                nextButton.backgroundColor = .orangeMain
-                nextButton.setTitleColor(.white, for: .normal)
-                nextButton.isEnabled = true
-            } else {
-                nextButton.backgroundColor = .gray2
-                nextButton.setTitleColor(.gray3, for: .normal)
-                nextButton.isEnabled = false
-            }
+        var text: String? = nil
+        let maxNumber = 5
+        text = textField.text
+        print("1: \(text)")
+        if maxNumber < (text?.count ?? 0) {
+            textField.text = text?.substring(from: 0, goto: maxNumber)
         }
+        print("2: \(textField.text)")
+//
+//        if let text = textField.text {
+//            if text.count > 5 {
+//                textField.deleteBackward()
+//            }
+//        }
+
+//        textField.text = textField.text?.trimmingCharacters(in: .whitespaces) // 스페이스 제거
+//        if let text = textField.text {
+//            if let regexText = filterNickName(text: text) {
+//                textField.text = regexText
+//            }
+//        }
+//
+//        if let count = textField.text?.count {
+//            if count > 0 && count < 6 {
+//                nextButton.backgroundColor = .orangeMain
+//                nextButton.setTitleColor(.white, for: .normal)
+//                nextButton.isEnabled = true
+//            } else {
+//                nextButton.backgroundColor = .gray2
+//                nextButton.setTitleColor(.gray3, for: .normal)
+//                nextButton.isEnabled = false
+//            }
+//        }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return true
+    }
+//
+//    func textField(_ textField: UITextField,
+//                   shouldChangeCharactersIn range: NSRange,
+//                   replacementString string: String) -> Bool {
+//        return self.textLimit(existingText: textField.text,
+//                              newText: string,
+//                              limit: 5)
+//    }
+
+}
+
+extension String {
+    func substring(from: Int, goto: Int) -> String {
+        guard from < count, goto >= 0, goto - from >= 0 else {
+            return ""
+        }
+
+        // Index 값 획득
+        let startIndex = index(self.startIndex, offsetBy: from)
+        let endIndex = index(self.startIndex, offsetBy: goto + 1) // '+1'이 있는 이유: endIndex는 문자열의 마지막 그 다음을 가리키기 때문
+
+        // 파싱
+        return String(self[startIndex ..< endIndex])
     }
 }
