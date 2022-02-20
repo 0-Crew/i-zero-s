c667bf0f2889fe5b7681445993f0e87ab7e99dfb
Juhyeoklee <gur6531@naver.com>
2022-02-21 00:21:44 +0900

    [:art: style] 린트 error 제거

--- Code-Zero/Global/Model/Constants.swift
+++ Code-Zero/Global/Model/Constants.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 struct Constants {
     static let userTermURL = "https://blog.naver.com/heey1792/222574483982"
     static let privacyTermURL = "https://blog.naver.com/heey1792/222580263334"
