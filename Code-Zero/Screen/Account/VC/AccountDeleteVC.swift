362523a50a27d445fee55d231c15faeb6ceaec8d
xwoud <xwoud@naver.com>
2022-02-20 17:05:19 +0900

    [:sparkles: feature] 탈퇴 확인 버튼 기능 추가
    body: 확인 버튼 클릭 시 alert 창이 없어지면서 처음 화면으로 이동
    
    feature: #43

--- Code-Zero/Screen/Account/VC/AccountDeleteVC.swift
+++ Code-Zero/Screen/Account/VC/AccountDeleteVC.swift
@@ -14,13 +14,32 @@ class AccountDeleteVC: UIViewController {
 
     // MARK: - @IBAction
     @IBAction func okButtonDidTap(_ sender: UIButton) {
-
+        // TODO: 계정 탈퇴 서버 연결
+        self.dismiss(animated: true) {
+            self.changeRootViewToHome()
+        }
     }
     @IBAction func cancleButtonDidTap(_ sender: UIButton) {
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        alertView.dropShadow(color: .black, offSet: CGSize(width: 0, height: 0), opacity: 0.15, radius: 10)
+        self.dismiss(animated: true, completion: nil)
     }
 
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        alertView.dropShadow(color: .black,
+                             offSet: CGSize(width: 0, height: 0),
+                             opacity: 0.15,
+                             radius: 10)
+    }
+
+    // MARK: - Function
+    private func changeRootViewToHome() {
+        let storybard = UIStoryboard(name: "Home", bundle: nil)
+        let homeNavigationVC = storybard.instantiateViewController(withIdentifier: "Home")
+        UIApplication.shared.windows.first?.replaceRootViewController(
+            homeNavigationVC,
+            animated: true,
+            completion: nil
+        )
+    }
 }
