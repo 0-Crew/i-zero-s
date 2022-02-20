a7ef814d6d687d5d4e41c539b236cd1894c3f82f
xwoud <xwoud@naver.com>
2022-02-20 16:23:43 +0900

    [:sparkles: feature] 백스페이스 감지
    body: shouldChangeCharactersIn 함수에서 백스페이스를 감지한 후 백스페이스시에도 글자수 제한 문구 띄어줌
    
    feat: #43

--- Code-Zero/Screen/Account/VC/AccountNickVC.swift
+++ Code-Zero/Screen/Account/VC/AccountNickVC.swift
@@ -21,15 +21,16 @@ class AccountNickVC: UIViewController {
         checkNickname()
     }
     @IBAction func logoutButtonDidTap(_ sender: UIButton) {
-
+        // TODO: 시작하기 화면으로 이동
     }
     @IBAction func deleteAccountButton(_ sender: UIButton) {
-
+        // TODO: 알랏창으로 이동
     }
 
     // MARK: - Property
     var email: String = "xwoud@naver.com"
     var nick: String = "희영룰루루"
+    var duplicateNick: [String] = ["민희", "주혁", "보틀월드"]
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -54,9 +55,16 @@ extension AccountNickVC {
             nickTextField.text = nick
         }
         if nickCount > 0 && nickCount <= 5 {
-            // 중복검사
+            guard let text = nickTextField.text else { return }
+            guard duplicateNick.contains(text) else {
+                self.view.endEditing(true)
+                // TODO: 서버에 닉네임 저장해야함
+                nick =  nickTextField.text!
+                editButton.setImage(UIImage(named: "icEditOrange"), for: .normal)
+                return
+            }
+            duplicateCheckLabel.text = "이미 사용 중이에요! 다른 닉네임을 적어주세요"
         }
-
     }
 }
 
@@ -65,20 +73,28 @@ extension AccountNickVC: UITextFieldDelegate {
         editButton.setImage(UIImage(named: "icCheckOrange"), for: .normal)
     }
     func textField(_ textField: UITextField,
-                       shouldChangeCharactersIn range: NSRange,
-                       replacementString string: String) -> Bool {
-            if string.isEmpty {
-                return true
-            }
-            switch string {
-            case "ㄱ"..."ㅎ", "a"..."z", "A"..."Z", "ㅏ"..."ㅣ", "0"..."9", "_":
-                let currentText = textField.text ?? ""
-                guard let stringRange = Range(range, in: currentText) else { return false }
-                let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
 
-                return updatedText.count <= 7
-            default:
-                return false
+        if let char = string.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b") // 백스페이스 감지
+            if isBackSpace == -92 {
+                duplicateCheckLabel.text = textField.text?.count ?? 0 > 6 ? "5자까지만 입력할 수 있어요" : ""
             }
         }
+
+        if string.isEmpty {
+            return true
+        }
+        switch string {
+        case "ㄱ"..."ㅎ", "a"..."z", "A"..."Z", "ㅏ"..."ㅣ", "0"..."9", "_":
+            let currentText = textField.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+            duplicateCheckLabel.text = updatedText.count > 5 ? "5자까지만 입력할 수 있어요!" : ""
+            return updatedText.count <= 7
+        default:
+            return false
+        }
+    }
 }
