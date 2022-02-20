6eee2482c84c27dc02744b813265c464f34ae442
himygit <43847166+Juhyeoklee@users.noreply.github.com>
2022-02-20 18:15:16 +0900

    온보딩4 (#44)
    * [:sparkles: feature] 온보딩 3단계 화면 구성
    
     feat: 온보딩3 #37
    
    * [:art: style] ChallengeView onboarding 상태 추가
    
    - 챌린지 뷰를 글로벌로 사용
    
    feat: 온보딩3 #37
    
    * [:art: style] Date 익스텐션 추가
    
    feat: 온보딩3 #37, 특정 날짜에서 n일 전, 후를 알 수 있는 Extension 제작 #18
    
    * [:art: style] ChallengeView 구조 개선
    
    - 온보딩 화면에 맞게 구조 개선
    
    feat: 온보딩3 #37
    
    * [:sparkles: feature] 현재 시간에 맞게 적용
    
    feat: 온보딩3 #37
    
    * [:sparkles: feature] 온보딩 4단계 lottie, 약관 적용
    
    feat: 온보딩4 #41
    
    * [:sparkles: feature] 챌린지 시작 버튼 적용
    
    feat: 온보딩4 #41
    
    * [:bulb: comment] 주석 추가
    
    feat: 온보딩4 #41
    
    * [:bulb: comment] 함수 이름 수정 및 주석 작성
    
    feat: 온보딩4 #41

---
+++ Code-Zero/Global/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Code-Zero
+//
+//  Created by 주혁 on 2022/02/06.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let userTermURL = "https://blog.naver.com/heey1792/222574483982"
+    static let privacyTermURL = "https://blog.naver.com/heey1792/222580263334"
+}
