eee3a0a661abd778e5900e9cc80a8a1ccf6f61f4
xwoud <xwoud@naver.com>
2022-02-21 01:45:27 +0900

    [:lipstick: layout] 애플 로그인 버튼 제작
    feat: #53

--- Code-Zero/Screen/SignUp/VC/SignInVC.swift
+++ Code-Zero/Screen/SignUp/VC/SignInVC.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class SignInVC: UIViewController {
 
     // MARK: - @IBOutlet
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var appleLoginView: UIView!
     @IBOutlet weak var subtitleLabel: UILabel!
 
     // MARK: - @IBAction
@@ -35,7 +37,23 @@ extension SignInVC {
         titleLabel.textAlignment = .left
         subtitleLabel.setLineSpacing(lineSpacing: 5)
         subtitleLabel.textAlignment = .left
+        addButton()
     }
+
+    func addButton() {
+        let button = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .black)
+            button.addTarget(self, action: #selector(loginHandler), for: .touchUpInside)
+        appleLoginView.addSubview(button)
+        }
+
+    @objc func loginHandler() {
+            let request = ASAuthorizationAppleIDProvider().createRequest()
+            request.requestedScopes = [.fullName, .email]
+            let controller = ASAuthorizationController(authorizationRequests: [request])
+            controller.delegate = self
+            controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+            controller.performRequests()
+        }
 
     func requestLogin() {
         UserLoginService.shared.requestLogin(id: "mini1234",
@@ -53,5 +71,20 @@ extension SignInVC {
                 print("serverErr")
             }
         }
+    }
+}
+extension SignInVC : ASAuthorizationControllerDelegate  {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let user = credential.user
+            print("👨‍🍳 \(user)")
+            if let email = credential.email {
+                print("✉️ \(email)")
+            }
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("error \(error)")
     }
 }
