b947e2e3392383f2cdf30413fa2301dcfd788804
xwoud <xwoud@naver.com>
2022-02-21 21:41:16 +0900

    [:sparkles: feature] 계정 공개 비공개 수정 서버 전달 기능 추가
    feat: #52

--- Code-Zero/Screen/Account/VC/AccountPrivacyVC.swift
+++ Code-Zero/Screen/Account/VC/AccountPrivacyVC.swift
@@ -23,7 +23,7 @@ class AccountPrivacyVC: UIViewController {
     // MARK: - Property
     var isPrivateSwitchOn: Bool = true {
         didSet {
-            // TODO: 서버 연결 해서 계정 범위 수정
+            requestToggleAccountPrivate()
         }
     }
 
@@ -31,5 +31,33 @@ class AccountPrivacyVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         privacySwitch.isOn = isPrivateSwitchOn
+    }
+}
+
+// MARK: - Network Function
+extension AccountPrivacyVC {
+    func requestToggleAccountPrivate() {
+        // swiftlint:disable line_length
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImVtYWlsIjoiZGRkZGRAbGRhc2tmai5jb20iLCJuYW1lIjoi7JWI64WV7ZWY7IqIIiwiaWRGaXJlYmFzZSI6IkRqZUZyS2tXWG5nS3lVc0JNVDdNa2E3TFlORTMiLCJpYXQiOjE2NDU0NDQzMDAsImV4cCI6MTY0ODAzNjMwMCwiaXNzIjoiV1lCIn0.7uraZqel7I0DVPX1El2I1gS2Hq13MdYJrrLSYPOsOvY"
+        UserDefaults.standard.set(token, forKey: "userToken")
+        // swiftlint:enable line_length
+
+        guard let token = UserDefaults.standard.string(forKey: "userToken") else { return }
+        UserPrivateService.shared.toggleAccountPrivate(token: token) { [weak self] result in
+            switch result {
+            case .success(let data):
+                if self?.isPrivateSwitchOn == data.isPrivate {
+                    // 토글이 반대로 설정
+                    self?.privacySwitch.isOn.toggle()
+                }
+            case .requestErr(let error):
+                print(error)
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+
+        }
     }
 }
