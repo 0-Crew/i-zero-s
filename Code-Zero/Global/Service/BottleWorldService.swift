2689a5e901446bb67bee4152cf947a5068049646
xwoud <xwoud@naver.com>
2022-02-27 17:00:36 +0900

    [:sparkles: feature] 보틀월드 둘러보기 데이터 가져올 수 있는 class 제작
    feat: #58

--- Code-Zero/Global/Service/BottleWorldService.swift
+++ Code-Zero/Global/Service/BottleWorldService.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Moya
+
+class BottleWorldService {
+    static let shared = BottleWorldService()
+    private lazy var service = MoyaProvider<APITarget>(plugins: [MoyaLoggingPlugin()])
+
+    public func requestBottleWoldBrowser(
+        token: String,
+        keyword: String?,
+        completion: @escaping (NetworkResult<BottleWorldUser>) -> Void
+    ) {
+        service.request(.bottleWorldBrowse(token: token,
+                                           keyworkd: keyword)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let decoder = JSONDecoder()
+                    let body = try decoder.decode(
+                        GenericResponse<BottleWorldUser>.self,
+                        from: response.data
+                    )
+                    if let data = body.data {
+                        completion(.success(data))
+                    }
+                } catch let error {
+                    debugPrint(error)
+                }
+            case .failure(let error):
+                completion(.serverErr)
+                debugPrint(error)
+            }
+        }
+    }
+}
