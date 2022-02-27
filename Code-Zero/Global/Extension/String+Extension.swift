e1d93810eeaa59806a6020d35adadc2a6ac5cf00
xwoud <xwoud@naver.com>
2022-02-27 17:54:09 +0900

    [:recycle: refactor] 날짜 포맷 바꾸는 기능 수정
    body: 기존 포맷이 여러개로 들어와서 기존 포맷도 받아주는 걸로 변경 ^-^..
    
    feat: #58

--- Code-Zero/Global/Extension/String+Extension.swift
+++ Code-Zero/Global/Extension/String+Extension.swift
@@ -8,16 +8,29 @@
 import Foundation
 
 extension String {
-    func recordDate(changeFormat: String) -> String {
-        // "yyyy-MM-dd HH:mm:ss"형식의 string 시간을 "yyyy년 MM월 dd일" 형식의 string time으로 바꾸기
-        var format = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    func recordDate(origin: String,
+                    change: String) -> String {
+        var format = origin
         let formatter = DateFormatter()
         formatter.dateFormat = format
         formatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
         guard let tempDate = formatter.date(from: self) else {
             return ""
         }
-        formatter.dateFormat = changeFormat
+        formatter.dateFormat = change
         return formatter.string(from: tempDate)
+    }
+
+    func toDate() -> Date? { 
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
+
+        }
+
     }
 }
