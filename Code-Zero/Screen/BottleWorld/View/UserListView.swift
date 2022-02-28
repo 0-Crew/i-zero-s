a15b6ad1e78eca317524b28d335e62a813dd5ae7
xwoud <xwoud@naver.com>
2022-02-28 16:02:41 +0900

    [:sparkles: feature] 팔로워 api 연결 후 데이터 적용
    body: 각 탭별로 값이 없을 때 다른 empty view가 뜨게 설정
    
    feat: #60

--- Code-Zero/Screen/BottleWorld/View/UserListView.swift
+++ Code-Zero/Screen/BottleWorld/View/UserListView.swift
@@ -12,7 +12,27 @@ import SnapKit
 class UserListView: UIView {
 
     // MARK: - Property
-    var userInfoData: [BottleWorldUser] = [] {
+    enum UserListTapType {
+        case lookAround
+        case follower
+        case following
+    }
+    var lookAroundUser: [BottleWorldUser] = []
+    var follower: [BottleWorldUser] = []
+    var following: [UserData] = []
+    var tapType: UserListTapType = .lookAround {
+        didSet {
+            switch tapType {
+            case .lookAround:
+                fetchBrowserData(keyword: nil)
+            case .follower:
+                fetchFollowerData(keyword: nil)
+            case .following:
+                break
+            }
+        }
+    }
+    var filteringText: String? { // 필터링 할 단어
         didSet {
             userListTableView.reloadData()
         }
@@ -25,6 +45,7 @@ class UserListView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadView()
+        makeDumyData()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -85,8 +106,15 @@ extension UserListView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UserListCell = tableView.dequeueCell(indexPath: indexPath)
         cell.delegate = self
-        cell.fetchUserData(data: userInfoData[indexPath.row])
-        cell.userId = userInfoData[indexPath.row].user.id
+        cell.cellIndex = indexPath // 나중에는 여기에 인덱스 대신에 나중에는 유저 고유 이메일 같은거가 들어가야 할 듯
+        switch tapType {
+        case .lookAround:
+            cell.fetchUserData(data: lookAroundUser[indexPath.row])
+        case .follower:
+            cell.fetchUserData(data: follower[indexPath.row])
+        case .following:
+            cell.setUserInfo(user: following[indexPath.row])
+        }
         return cell
     }
 }
@@ -110,7 +138,6 @@ extension UserListView {
                              term: "12/13-20", follow: true)
         let data8 = UserData(name: "미니테스트중", bottleLevel: 7, subject: nil,
                              term: nil, follow: true)
-        follower = [data1, data4, data5, data7, data8]
         following = [data2, data3, data6]
     }
     private func fetchBrowserData(keyword: String?) {
@@ -137,12 +164,45 @@ extension UserListView {
                 }
             }
     }
+    private func fetchFollowerData(keyword: String?) {
+        // swiftlint:disable line_length
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsImVtYWlsIjoieTR1cnRpam5makBwcml2YXRlcmVsYXkuYXBwbGVpZC5jb20iLCJuYW1lIjoi7JWg7ZSM6rmA66-87Z2sIiwiaWRGaXJlYmFzZSI6IkpoaW16VDdaUUxWcDhmakx3c1U5eWw1ZTNaeDIiLCJpYXQiOjE2NDU5NDcwNzksImV4cCI6MTY0ODUzOTA3OSwiaXNzIjoiV1lCIn0.4c_MKEolk5Mv5GOjJbQxcAkwpJLyyOTX_fVptT_0sO4"
+        // swiftlint:enable line_length
+        BottleWorldService
+            .shared
+            .requestBottleWoldFollower(token: token, keyword: keyword) { [weak self] result in
+                switch result {
+                case .success(let userData):
+                    self?.follower = userData
+                    if userData.count == 0 {
+                        self?.delegate?.presentEmptyUserView()
+                        break
+                    }
+                    self?.delegate?.presentUserListView()
+                    self?.userListTableView.reloadData()
+                case .requestErr(let error):
+                    print(error)
+                case .serverErr:
+                    break
+                case .networkFail:
+                    break
+                }
+            }
+    }
 }
 
 // MARK: - UserListCellDelegate
 extension UserListView: UserListCellDelegate {
     func didFollowButtonTap(id index: Int) {
         // 팔로우, 팔로잉 서버 연결 코드 작성 예정
+        switch tapType {
+        case .lookAround:
+            break
+        case .follower:
+            break
+        case .following:
+            following[index].follow = !following[index].follow
+        }
         userListTableView.reloadData()
     }
 }
