47140bb3e2add1e86ff0340232687951fa1554e2
xwoud <xwoud@naver.com>
2022-02-28 21:11:09 +0900

    [:sparkles: feature] 닉네임 변경 API Service 제작
    feat: #51

---
+++ Code-Zero/Global/Service/UserSettingService.swift
@@ -0,0 +1,40 @@
+//
+//  UserSettingService.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2022/02/28.
+//
+
+import Foundation
+import Moya
+
+class UserSettingService {
+    static let shared = UserSettingService()
+    private lazy var service = MoyaProvider<APITarget>(plugins: [MoyaLoggingPlugin()])
+
+    public func changeUserNick(
+        nick: String,
+        token: String,
+        completion: @escaping (NetworkResult<SimpleData>) -> Void
+    ) {
+        service.request(.userNick(nick: nick,
+                                  token: token)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let decoder = JSONDecoder()
+                    let body = try decoder.decode(
+                        SimpleData.self,
+                        from: response.data
+                    )
+                    completion(.success(body))
+                } catch let error {
+                    debugPrint(error)
+                }
+            case .failure(let error):
+                completion(.serverErr)
+                debugPrint(error)
+            }
+        }
+    }
+}
