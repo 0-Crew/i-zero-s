f87d7bfddeaeaa2996c3759401b9f56666040893
minnit <51286963+xwoud@users.noreply.github.com>
2022-05-26 10:44:03 +0900

    설정 화면 스크립션 수정 (#67)
    * [✨ sparkles] 설정화면에서 유저 정보 받아오는 network case 추가
    
    feat: #66
    
    * [:sparkles: feature] 추가한 UserInfo 받아오는 네트워크 구조체를 이용하여 Static Class 생성
    
    feat: #66
    
    * [:zap: improve] UIBarButtonItem 의 touch action이 먹히지 않아 lazy 상태로 변경
    
    feat: #66
    
    * [:sparkles: feature] 홈 화면에서 햄버거 바 클릭 시 설정 화면으로 이동하는 기능 추가
    
    feat: #66
    
    * [:sparkles: feature] 닉네임 클릭 시 계정 설정 화면으로 이동
    body: 이전에는 버튼을 제작해 닉네임만 클릭 시 이동하였지만, 클릭 범위가 늘어남에 따라 UIView자체에 tap gesture를 주는 형식으로 변경
    
    feat: #66
    
    * [:lipstick: layout] button에서 Label로 변경
    
    * [:lipstick: layout] 닉네임 1글자를 보여주는 로직 수정
    body: 맨 앞 글자가 _인 경우에는 두번째 글자를 보여주는 로직추가
    
    feat: #66
    
    * [:lipstick: layout] storyboard 상의 layout 변경
    
    * [:sparkles: feature] 설정에서 각 탭 클릭 시 외부 사이트로 연결 기능 추가
    body: 인스타그램 제외 링크 변경 필요
    
    feat: #66
    
    * [:art: style] 닉네임을 홈 화면에서 전달 받은 후 적용
    
    feat: #66
    
    * [:sparkles: feature] 홈 화면에서 설정 화면 개인 정보 불러오는 네트워크 연결
    body: 설정 화면이 아니라 홈 화면에서 네트워크를 불러온 이유는 홈 화면에서 사용자의 닉네임을 title로 사용해야하기 때문
    또한 설정 화면에서 닉네임 변경 후 dismiss로 돌아왔을 때 다시 불러오기 위해 viewWillAppear에 코드 작성
    
    feat: #66
    
    * [:art: style] Lint 에 맞춰 에러 수정
    
    * [:memo: docs] 외부 링크 수정
    
    * [:bug: bug] 500 error status code를 잡아내는 코드 수정
    
    * [:bulb: comment] 린트 주석 추가
    
    * [:art: style] URL 웹 사이트로 연결하는 Extension 적용
    
    feat: #66

--- Code-Zero/Screen/Challenge/VC/ChallengeVC.swift
+++ Code-Zero/Screen/Challenge/VC/ChallengeVC.swift
@@ -23,19 +23,21 @@ class ChallengeVC: UIViewController {
             updateSocialButtons(offset: selectedPersonIndex)
         }
     }
+    private var userInfo: UserInfo?
 
     // MARK: - UI Components
-    private let menuButton: UIBarButtonItem = {
+    private lazy var alarmButton: UIBarButtonItem = {
         let button: UIButton = .init(type: .custom)
         button.frame = .init(x: 0, y: 0, width: 24, height: 24)
         button.setImage(UIImage(named: "icAlarm"), for: .normal)
         let barButtonItem = UIBarButtonItem(customView: button)
         return barButtonItem
     }()
-    private let alarmButton: UIBarButtonItem = {
+    private lazy var menuButton: UIBarButtonItem = {
         let button: UIButton = .init(type: .custom)
         button.frame = .init(x: 0, y: 0, width: 24, height: 24)
         button.setImage(UIImage(named: "icMenu"), for: .normal)
+        button.addTarget(self, action: #selector(menuButtonDidTap), for: .touchUpInside)
         let barButtonItem = UIBarButtonItem(customView: button)
         return barButtonItem
     }()
@@ -58,6 +60,11 @@ class ChallengeVC: UIViewController {
         highlightingFollowingListButton(offset: selectedPersonIndex)
         updateSocialButtons(offset: selectedPersonIndex)
     }
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+        fetchUserInfoData()
+    }
+
     // MARK: - IBAction Method
 
     @IBAction func cheerUpButtonDidTap(_ sender: Any) {
@@ -151,7 +158,7 @@ extension ChallengeVC {
         let space = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
         space.width = 8
         self.navigationItem.setRightBarButtonItems(
-            [alarmButton, space, menuButton],
+            [menuButton, space, alarmButton],
             animated: false
         )
     }
@@ -213,5 +220,49 @@ extension ChallengeVC {
         let isChallenging = followingPeopleChallengingLists[offset].isChallenging
         cheerUpButton.isHidden = (isChallenging == false) || isMine
         followingButton.isHidden = isMine
+    }
+}
+
+// MARK: - Move Controller
+extension ChallengeVC {
+    @objc private func menuButtonDidTap() {
+        let storybard = UIStoryboard(name: "Setting", bundle: nil)
+        guard let settingVC = storybard.instantiateViewController(withIdentifier: "SettingVC")
+                as? SettingVC else { return }
+        settingVC.userInfo = userInfo
+        navigationController?.pushViewController(settingVC, animated: true)
+    }
+    private func changeRootViewToHome() {
+        let storybard = UIStoryboard(name: "Home", bundle: nil)
+        let homeNavigationVC = storybard.instantiateViewController(withIdentifier: "Home")
+        UIApplication.shared.windows.first?.replaceRootViewController(
+            homeNavigationVC,
+            animated: true,
+            completion: nil
+        )
+    }
+}
+
+// MARK: - Network
+extension ChallengeVC {
+    private func fetchUserInfoData() {
+        // swiftlint:disable line_length
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsImVtYWlsIjoieTR1cnRpam5makBwcml2YXRlcmVsYXkuYXBwbGVpZC5jb20iLCJuYW1lIjoi67SJ6rWs7Iqk67Cl66mNIiwiaWRGaXJlYmFzZSI6IkpoaW16VDdaUUxWcDhmakx3c1U5eWw1ZTNaeDIiLCJpYXQiOjE2NTM0ODk4MTAsImV4cCI6MTY1NjA4MTgxMCwiaXNzIjoiV1lCIn0.5oevdqhJA_NhURaD3-OOCwbUE92GvcXDndAFPW3vOHE"
+        // swiftlint:enable line_length
+        UserInfoService.shared.requestLogin(token: token) { [weak self] result in
+            switch result {
+            case .success(let info):
+                self?.userInfo = info.user
+                self?.nickNameLabel.text = info.user.name
+            case .requestErr(let error):
+                print(error)
+            case .serverErr:
+                // 토큰 만료(자동 로그아웃 느낌..)
+                self?.changeRootViewToHome()
+            case .networkFail:
+                // TODO: 서버 자체 에러 - 서버 점검 중 popup 제작?
+                break
+            }
+        }
     }
 }
