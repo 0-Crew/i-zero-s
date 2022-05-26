d6e2319c29a0ecf0a9bc6ce147f9f8960c1cc0f3
Juhyeoklee <gur6531@naver.com>
2022-05-27 00:28:17 +0900

    [:lipstick: layout] 알람센터 empty view 설정
    feat: 알림센터 알림유/무 버전 #63

--- Code-Zero/Screen/AlarmCenter/VC/AlarmCenterVC.swift
+++ Code-Zero/Screen/AlarmCenter/VC/AlarmCenterVC.swift
@@ -7,55 +7,110 @@
 
 import UIKit
 
+import SnapKit
+
 class AlarmCenterVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
-    var alarms: [String] = [
-        "박수빈빈빈님의 보틀을 ",
-        "박수빈빈빈님이 새로운 챌린지를 시작했어요!",
-        "가니가니가님이 내 챌린지 성공을 축하해요!",
-        "가니가니가님이 내 챌린지 성공을 축하해요!",
-        "가니가니가님이 내 챌린지 성공을 축하해요!",
-        "박수빈빈빈님이 새로운 시작했어요!",
-        "박수빈빈빈님이 새로운 시작했어요!",
-        "박수빈빈빈님이 새로운 챌린지를 시작했어요!"
-    ]
+    lazy var emptyView: UIView = {
+        let view = UIView(frame: .zero)
+        let backgroundView = UIView(frame: .zero)
+        let imageView = UIImageView(image: UIImage(named: "imgAlarmNone"))
 
-    var types: [AlarmType] = [
-        .cheerUp,
-        .beCelebrated,
-        .beCheered,
-        .cheerUp,
-        .celebrate,
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "오늘은 알림이 없습니다"
+            label.font = .spoqaHanSansNeo(size: 16, family: .bold)
+            label.textColor = .gray4
+            label.textAlignment = .center
+            return label
+        }()
+        let subtitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "새로운 소식이 생기면 알려드릴게요!"
+            label.font = .spoqaHanSansNeo(size: 14, family: .medium)
+            label.textColor = .gray4
+            label.textAlignment = .center
+            return label
+        }()
+
+        backgroundView.addSubview(imageView)
+        backgroundView.addSubview(titleLabel)
+        backgroundView.addSubview(subtitleLabel)
+        view.addSubview(backgroundView)
+
+        imageView.snp.makeConstraints {
+            $0.width.height.equalTo(90)
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+        }
+
+        subtitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+
+        backgroundView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        view.backgroundColor = .white
+
+        return view
+    }()
+    // alarms가 빈배열일 경우 emptyView 노출
+    var alarms: [(String, AlarmType)] = [
+        ("박수빈빈빈님의 보틀을 ", .cheerUp),
+        ("박수빈빈빈님이 새로운 챌린지를 시작했어요!", .beCelebrated),
+        ("가니가니가님이 내 챌린지 성공을 축하해요!", .beCheered),
+        ("가니가니가님이 내 챌린지 성공을 축하해요!", .cheerUp),
+        ("가니가니가님이 내 챌린지 성공을 축하해요!", .cheerUp),
+        ("박수빈빈빈님이 새로운 시작했어요!", .cheerUp),
+        ("박수빈빈빈님이 새로운 시작했어요!", .cheerUp),
+        ("박수빈빈빈님이 새로운 챌린지를 시작했어요!", .cheerUp)
     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
+        if alarms.count == 0 {
+            setEmptyView()
+        }
     }
 
     private func setNavigationBar() {
         self.navigationItem.title = "알림"
     }
+
+    private func setEmptyView() {
+        view.addSubview(emptyView)
+        emptyView.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalTo(view)
+        }
+    }
 }
 
 extension AlarmCenterVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return types.count
+        return alarms.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: AlarmCell = tableView.dequeueCell(indexPath: indexPath)
         cell.delegate = self
-        cell.bindData(type: types[indexPath.item], text: alarms[indexPath.item])
+        cell.bindData(type: alarms[indexPath.item].1, text: alarms[indexPath.item].0)
         return cell
     }
 }
 
 extension AlarmCenterVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cellType = types[indexPath.item]
+        let cellType = alarms[indexPath.item].1
         switch cellType {
         case .beCheered, .beCelebrated:
             guard
