d528de869305718a1494c07b1e1c8297dec08859
xwoud <xwoud@naver.com>
2022-05-29 19:25:01 +0900

    [:sparkles: feature] HomeVC에도 로그인 연결 body: 테스트 할 때를 위해 주석 처리 해둠
    feat: #72

--- Code-Zero/Screen/Home/VC/HomeVC.swift
+++ Code-Zero/Screen/Home/VC/HomeVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AuthenticationServices
+import KakaoSDKUser
 
 class HomeVC: UIViewController {
 
@@ -30,10 +32,12 @@ class HomeVC: UIViewController {
     }
 
     @IBAction func kakaoSignInButtonDidTap() {
-        changeRootViewToChallenge()
+//        kakaoLoginViewDidTap()
+        moveChallengeVC()
     }
     @IBAction func appleSignInButtonDidTap() {
-        changeRootViewToChallenge()
+//        appleLoginViewDidTap()
+        moveChallengeVC()
     }
 
 }
@@ -46,13 +50,113 @@ extension HomeVC {
 
         onboardingButton.setBorder(borderColor: .white, borderWidth: 1.0)
     }
-    private func changeRootViewToChallenge() {
+}
+
+// MARK: - Login Function
+extension HomeVC {
+    private func requestLogin(id: String, token: String, provider: String) {
+        UserLoginService.shared.requestLogin(id: id,
+                                             token: token,
+                                             provider: provider) { [weak self] result in
+            switch result {
+            case .success(let data):
+                data.type == "login" ? self?.moveChallengeVC() : self?.moveNickSettingVC()
+            case .requestErr(let error):
+                print(error)
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("serverErr")
+            }
+        }
+    }
+    private func moveChallengeVC() {
         let storybard = UIStoryboard(name: "Challenge", bundle: nil)
-        let challengeNavigationVC = storybard.instantiateViewController(withIdentifier: "Challenge")
+        let challengeVC = storybard.instantiateViewController(withIdentifier: "Challenge")
         UIApplication.shared.windows.first?.replaceRootViewController(
-            challengeNavigationVC,
+            challengeVC,
             animated: true,
             completion: nil
         )
+    }
+    private func moveNickSettingVC() {
+        guard let nickSettingVC = storyboard?.instantiateViewController(withIdentifier: "NickSettingVC")
+        else { return }
+        navigationController?.pushViewController(nickSettingVC, animated: true)
+    }
+}
+
+// MARK: - KaKao
+extension HomeVC {
+    private func kakaoLoginViewDidTap() {
+        switch UserApi.isKakaoTalkLoginAvailable() {
+        case true:
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error) }
+                if let oauthToken = oauthToken {
+                    self.kakaoLoginSuccess(token: oauthToken.accessToken)
+                }
+            }
+        case false:
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error) }
+                if let oauthToken = oauthToken {
+                    self.kakaoLoginSuccess(token: oauthToken.accessToken)
+                }
+            }
+        }
+    }
+    private func kakaoLoginSuccess(token: String) {
+        UserApi.shared.me() {(user, error) in
+            if let error = error {
+                print(error) }
+            if let user = user {
+                self.requestLogin(id: "\(user.id ?? 0)", token: token, provider: "kakao")
+            }
+        }
+    }
+}
+
+// MARK: - ASAuthorizationControllerDelegate & Apple
+extension HomeVC: ASAuthorizationControllerDelegate {
+    private func appleLoginViewDidTap() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        as? ASAuthorizationControllerPresentationContextProviding
+        controller.performRequests()
+    }
+    func authorizationController(controller: ASAuthorizationController,
+                                 didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential,
+           let identityToken = credential.identityToken,
+           let tokenString = String(data: identityToken, encoding: .utf8) {
+            requestLogin(id: credential.user,
+                         token: tokenString,
+                         provider: "apple")
+            UserDefaults.standard.set(credential.user,
+                                      forKey: "appleId")
+        }
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        guard let error = error as? ASAuthorizationError else {return}
+        switch error.code {
+        case .canceled:
+            print("Canceled")
+        case .unknown:
+            print("Unknow")
+        case .invalidResponse:
+            print("invalid Respone")
+        case .notHandled:
+            print("Not Handled")
+        case .failed:
+            print("Failed")
+        default:
+            print("Default")
+        }
     }
 }
