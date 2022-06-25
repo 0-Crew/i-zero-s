294d048017791d8e9eebf7724b25733521972ca5
minnit <51286963+xwoud@users.noreply.github.com>
2022-06-26 00:52:10 +0900

    카카오 로그인 연동 (#76)
    * [:lipstick: layout] kakao 로고 이미지 추가
    
    feat: #72
    
    * [:card_file_box: database] lint 제외 문자열 추가
    
    feat: #72
    
    * [:sparkles: feature] 카카오 로그인 시 카카오톡 화면에서 다시 우리 어플로 돌아올 때를 위해 URLContexts 함수 추가
    
    feat: #72
    
    * [:sparkles: feature] Kakao 네이티브 앱 키 추가
    
    feat: #72
    
    * [:lipstick: layout] 카카오 로그인 버튼 생성
    
    * [:sparkles: feature] 카카오 로그인 후 데이터 받아서 서버에 전송
    
    feat: #72
    
    * [:sparkles: feature] HomeVC에도 로그인 연결
    body: 테스트 할 때를 위해 주석 처리 해둠
    
    feat: #72
    
    * [:sparkles: feature] 카카오 로그인 및 애플 로그인 기능 추가

--- Code-Zero/Support/Delegate/SceneDelegate.swift
+++ Code-Zero/Support/Delegate/SceneDelegate.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 import AuthenticationServices
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if AuthApi.isKakaoTalkLoginUrl(url) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
 
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
@@ -48,5 +57,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 }
