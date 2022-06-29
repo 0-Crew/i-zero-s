61525bdaf90eca80dba5633a3e3387febacf5330
xwoud <xwoud@naver.com>
2022-06-29 18:15:00 +0900

    [:lipstick: layout] 블러뷰를 white 뷰로 변경
    feat: #75

--- Code-Zero/Global/View/HalfModalPresentationController.swift
+++ Code-Zero/Global/View/HalfModalPresentationController.swift
@@ -2,20 +2,20 @@
 //  HalfModalPresentationController.swift
 //  Code-Zero
 //
-//  Created by Hailey on 2022/06/02.
+//  Created by 김민희 on 2022/06/02.
 //
 
 import UIKit
 
 class HalfModalPresentationController: UIPresentationController {
     
-    let blurEffectView: UIVisualEffectView!
     var backTapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
+    let blurEffectView: UIView!
     var check: Bool = false
     
     override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
-        let blurEffect = UIBlurEffect(style: .dark)
-        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView = UIView()
+        blurEffectView.backgroundColor = .white
         super.init(presentedViewController: presentedViewController, presenting: presentedViewController)
         backTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissController))
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
@@ -31,18 +31,18 @@ class HalfModalPresentationController: UIPresentationController {
                             height: self.containerView!.frame.height * 619 / 800))
     } 
     
-    // 모달이 올라갈 때 뒤에 있는 배경을 검은색 처리해주는 용도
     override func presentationTransitionWillBegin() {
-        self.blurEffectView.alpha = 0
         self.containerView!.addSubview(blurEffectView)
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0.7},
-                                                                    completion: nil)
     }
     
     // 모달이 없어질 때 검은색 배경을 슈퍼뷰에서 제거
     override func dismissalTransitionWillBegin() {
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in self.blurEffectView.alpha = 0},
-                                                                    completion: { _ in self.blurEffectView.removeFromSuperview()})
+        self.presentedViewController.transitionCoordinator?.animate(
+            alongsideTransition: { _ in
+                self.blurEffectView.alpha = 0 },
+            completion: { _ in
+                self.blurEffectView.removeFromSuperview()
+            })
     }
     
     // 모달의 크기가 조절됐을 때 호출되는 함수
