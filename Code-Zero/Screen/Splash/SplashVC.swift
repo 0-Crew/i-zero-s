794f594a876166e26a3048e1e2e87e7b5f762784
xwoud <xwoud@naver.com>
2022-07-05 00:56:53 +0900

    [:sparkles: feature] Lottie 화면에서 1바퀴 돌아간 후 token 의 유무를 체크하여 root View를 변경
    feat: #94

--- Code-Zero/Screen/Splash/SplashVC.swift
+++ Code-Zero/Screen/Splash/SplashVC.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import Lottie
+import SnapKit
 
 class SplashVC: UIViewController {
 
+    // MARK: - Property
+    private let animationView = AnimationView(name: "loading")
+
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orangeMain
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        settingSplashLottie()
     }
-    */
+
+    // MARK: - Set Lottie
+    private func settingSplashLottie() {
+        animationView.contentMode = .scaleToFill
+        view.addSubview(animationView)
+        animationView.snp.makeConstraints {
+            $0.width.equalTo(120)
+            $0.height.equalTo(120)
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+    private func runSplashLottie() {
+        animationView.play(fromProgress: 0,
+                           toProgress: 1,
+                           loopMode: LottieLoopMode.playOnce,
+                           completion: { (finished) in
+            switch finished {
+            case true:
+                self.checkIsUser()
+            case false:
+                self.runSplashLottie()
+            }
+        })
+    }
+
+    // MARK: - Check User
+    private func checkIsUser() {
+        let isUser: Bool = UserDefaultManager.shared.accessToken != nil
+        changeRootViewController(isUser)
+    }
+    private func changeRootViewController(_ isUser: Bool) {
+        let root: String = isUser ? "Challenge" : "Home"
+        let storybard = UIStoryboard(name: root, bundle: nil)
+        let viewController = storybard.instantiateViewController(withIdentifier: root)
+        UIApplication.shared.windows.first?.replaceRootViewController(
+            viewController,
+            animated: true,
+            completion: nil
+        )
+    }
 
 }
