af70be294a115b5251836e72a43a902d75c97a72
xwoud <xwoud@naver.com>
2022-07-05 00:55:37 +0900

    [:lipstick: layout] Splash 관련 Storyboard 및 ViewController 생성
    feat: #94

---
+++ Code-Zero/Screen/Splash/SplashVC.swift
@@ -0,0 +1,29 @@
+//
+//  SplashVC.swift
+//  Code-Zero
+//
+//  Created by 미니 on 2022/07/05.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
