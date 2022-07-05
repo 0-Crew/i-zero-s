ed30cf9232b0162b3d30dae6f4587933eb343dd4
xwoud <xwoud@naver.com>
2022-07-05 17:33:56 +0900

    [:fire: remove] 사용하지 않는 코드 삭제
    feat: #75

--- Code-Zero/Screen/Calendar/VC/CalendarVC.swift
+++ Code-Zero/Screen/Calendar/VC/CalendarVC.swift
@@ -68,8 +68,6 @@ class CalendarVC: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
-        view.addGestureRecognizer(panGesture)
         setView()
         setChallengeJoinView()
         makeButton()
@@ -83,27 +81,6 @@ class CalendarVC: UIViewController {
             pointOrigin = self.view.frame.origin
         }
     }
-    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
-            let translation = sender.translation(in: view)
-            
-            // Not allowing the user to drag the view upward
-            guard translation.y >= 0 else { return }
-            
-            // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
-            view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
-            
-            if sender.state == .ended {
-                let dragVelocity = sender.velocity(in: view)
-                if dragVelocity.y >= 1300 {
-                    self.dismiss(animated: true, completion: nil)
-                } else {
-                    // Set back to original position of the view controller
-                    UIView.animate(withDuration: 0.3) {
-                        self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
-                    }
-                }
-            }
-        }
 }
 
 // MARK: - View Function
