fc1e87fd148b10b91a822c8bcd990654adf76992
xwoud <xwoud@naver.com>
2022-07-06 12:48:54 +0900

    [:fire: remove] Lottie 사용 안하기로 해서 관련 코드 및 파일 삭제
    feat: #94

--- Code-Zero/Screen/Splash/SplashVC.swift
+++ Code-Zero/Screen/Splash/SplashVC.swift
@@ -6,52 +6,17 @@
 //
 
 import UIKit
-import Lottie
-import SnapKit
 
 class SplashVC: UIViewController {
-
-    // MARK: - Property
-    private let animationView = AnimationView(name: "splash")
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orangeMain
     }
-    override func viewDidAppear(_ animated: Bool) {
-        settingSplashLottie()
-    }
-
-    // MARK: - Set Lottie
-    private func settingSplashLottie() {
-        animationView.contentMode = .scaleToFill
-        view.addSubview(animationView)
-        animationView.snp.makeConstraints {
-            $0.width.equalTo(120)
-            $0.height.equalTo(120)
-            $0.centerX.centerY.equalToSuperview()
-        }
-        runSplashLottie()
-    }
-    private func runSplashLottie() {
-        animationView.play(fromProgress: 0,
-                           toProgress: 1,
-                           loopMode: LottieLoopMode.playOnce,
-                           completion: { (finished) in
-            switch finished {
-            case true:
-                self.checkIsUser()
-            case false:
-                self.runSplashLottie()
-            }
-        })
-    }
 
     // MARK: - Check User
     private func checkIsUser() {
-        let isUser: Bool = UserDefaultManager.shared.accessToken != nil
-        changeRootViewController(isUser)
     }
     private func changeRootViewController(_ isUser: Bool) {
         let root: String = isUser ? "Challenge" : "Home"
