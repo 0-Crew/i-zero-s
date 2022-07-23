4c45a22430dd34785198b32f92630869ca79dab5
xwoud <xwoud@naver.com>
2022-07-23 17:53:29 +0900

    [:sparkles: feature] 카카오 로그인 유저일 시 토큰 refresh
    feat: #98

--- Code-Zero/Screen/Splash/SplashVC.swift
+++ Code-Zero/Screen/Splash/SplashVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class SplashVC: UIViewController {
 
@@ -13,10 +15,29 @@ class SplashVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orangeMain
-        checkIsUser()
+        kakaoTokenRefresh()
     }
 
     // MARK: - Check User
+    private func kakaoTokenRefresh() {
+        guard let provider = provider,
+              provider == "kakao" else {
+            checkIsUser()
+            return
+        }
+        if AuthApi.hasToken() {
+            UserApi.shared.accessTokenInfo { (_, error) in
+                if error != nil {
+                    UserDefaultManager.shared.removeAccessToken()
+                    UserDefaultManager.shared.removeProvider()
+                    self.checkIsUser()
+                } else {
+                    // 토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                    self.checkIsUser()
+                }
+            }
+        }
+    }
     private func checkIsUser() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
             let isUser: Bool = UserDefaultManager.shared.accessToken != nil
@@ -33,5 +54,4 @@ class SplashVC: UIViewController {
             completion: nil
         )
     }
-
 }
