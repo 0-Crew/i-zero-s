e33bf84447ef0448211afcf50d1ba0b9ff5134ae
xwoud <xwoud@naver.com>
2022-07-23 17:53:56 +0900

    [:sparkles: feature] 로그인 시 provider 저장
    feat: #98

--- Code-Zero/Screen/Home/VC/HomeVC.swift
+++ Code-Zero/Screen/Home/VC/HomeVC.swift
@@ -59,7 +59,7 @@ extension HomeVC {
             switch result {
             case .success(let data):
                 UserDefaultManager.shared.saveAccessToken(accessToken: data.accesstoken)
-                UserDefaultManager.shared.saveProvider(provider: data.type)
+                UserDefaultManager.shared.saveProvider(provider: provider)
                 data.type == "login" ? self?.moveChallengeVC() : self?.moveNickSettingVC()
             case .requestErr(let error):
                 print(error)
